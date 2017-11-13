70bb5bf4f39e0b162ebb0c0b10a14c4ed457cdc6
Lewis MacNee <lewis.macnee@hotmail.com>
2017-11-13 18:05:55 +0000

    added bezier Curve creators

--- project/public/app.js
+++ project/public/app.js
@@ -12,6 +12,19 @@ var drawHeight = 500
 context.strokeRect(50, 50, drawWidth, drawHeight)
 context.strokeRect(0, 0, width, height)
 
+//start of drawing;
+context.beginPath();
+context.moveTo(100, 300);
+//start of bezier curve drawing;
+context.bezierCurveTo(200, 250, 300, 250, 600, 240);
+context.lineTo(620, 330)
+context.bezierCurveTo(200, 320, 150, 320, 100, 300)
+
+
+//end drawing
+context.stroke();
+
+
 // console.log(drawWidth + " " + drawHeight)
 
 window.onload = function(){
